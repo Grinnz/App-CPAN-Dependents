1e4e357d8884b4f8b72eb7d99a93edffccd57fa6
Dan Book <grinnz@grinnz.com>
2015-05-26 12:08:46 -0400

    Bump version

--- lib/App/CPAN/Dependents.pm
+++ lib/App/CPAN/Dependents.pm
@@ -8,7 +8,7 @@ use HTTP::Tiny;
 use JSON::Tiny 'decode_json', 'encode_json';
 use URI::Escape 'uri_escape';
 
-our $VERSION = '0.003';
+our $VERSION = '0.004';
 
 our @EXPORT_OK = ('find_all_dependents');
 
